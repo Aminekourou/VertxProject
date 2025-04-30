2f86d8adc4c4528cb4aaece049b0d18c14c8a0f0
Aminekourou <125512385+Aminekourou@users.noreply.github.com>
2025-04-30 16:48:56 +0000

    comm

---
+++ src/Routes/SinglePostPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SinglePostPage() {
+  return (
+    <div className=""></div>
+  );
+}
+
+export default SinglePostPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,49 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+ 
+} from "react-router-dom";
+import Homepage from './Routes/Homepage.jsx';
+import PostListPage from './Routes/PostListPage.jsx';
+import Write from './Routes/Write.jsx';
+import LoginPage from './Routes/LoginPage.jsx';
+import RegisterPage from './Routes/RegisterPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Homepage />
+      
+    ),
+  },
+  {
+    path: "/posts",
+    element: <PostListPage />,
+  },
+  {
+    path: "/:slug",
+    element: <PostListPage />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/:slug",
+    element: <RegisterPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
